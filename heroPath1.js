f0df46c3781b95237d2920960a09e640a9040787
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-10-16 08:45:00 -0300

    Update HeroPath

--- heroPath1.js
+++ heroPath1.js
@@ -6,21 +6,21 @@ const xpHeroi = 7500; // Substitua pela quantidade de experiência do herói
 let nivelHeroi;
 
 if (xpHeroi < 1000) {
-  nivelHeroi = "Ferro";
+    nivelHeroi = "Ferro";
 } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
-  nivelHeroi = "Bronze";
+    nivelHeroi = "Bronze";
 } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
-  nivelHeroi = "Prata";
+    nivelHeroi = "Prata";
 } else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
-  nivelHeroi = "Ouro";
+    nivelHeroi = "Ouro";
 } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
-  nivelHeroi = "Platina";
+    nivelHeroi = "Platina";
 } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
-  nivelHeroi = "Ascendente";
+    nivelHeroi = "Ascendente";
 } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
-  nivelHeroi = "Imortal";
+    nivelHeroi = "Imortal";
 } else {
-  nivelHeroi = "Radiante";
+    nivelHeroi = "Radiante";
 }
 
 // Exibir a mensagem com o nome e o nível do herói
